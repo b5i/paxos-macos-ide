9f42f8c180398954c822ae450670b2456fb6aec3
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2023-09-05 20:08:46 +0200

    Initial Commit

---
+++ PaxOSLua/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaxOSLua
+//
+//  Created by Antoine Bollengier on 05.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaxOSLua/PaxOSLuaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaxOSLuaApp.swift
+//  PaxOSLua
+//
+//  Created by Antoine Bollengier on 05.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PaxOSLuaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
